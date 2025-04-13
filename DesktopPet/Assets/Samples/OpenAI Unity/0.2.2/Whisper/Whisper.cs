7cfe24afb7ce11973be1898ea958dddb6d9bba58
Homan Qiu <thehuiq2006@gmail.com>
2025-04-13 02:46:05 -0700

    Short Update

--- DesktopPet/Assets/Samples/OpenAI Unity/0.2.2/Whisper/Whisper.cs
+++ DesktopPet/Assets/Samples/OpenAI Unity/0.2.2/Whisper/Whisper.cs
@@ -9,8 +9,6 @@ namespace Samples.Whisper
         [SerializeField] private Button recordButton;
         [SerializeField] private Image progressBar;
         [SerializeField] private Text message;
-        [SerializeField] private Dropdown dropdown;
-        
         private readonly string fileName = "output.wav";
         private readonly int duration = 5;
         
@@ -21,19 +19,7 @@ namespace Samples.Whisper
 
         private void Start()
         {
-            #if UNITY_WEBGL && !UNITY_EDITOR
-            dropdown.options.Add(new Dropdown.OptionData("Microphone not supported on WebGL"));
-            #else
-            foreach (var device in Microphone.devices)
-            {
-                dropdown.options.Add(new Dropdown.OptionData(device));
-            }
             recordButton.onClick.AddListener(StartRecording);
-            dropdown.onValueChanged.AddListener(ChangeMicrophone);
-            
-            var index = PlayerPrefs.GetInt("user-mic-device-index");
-            dropdown.SetValueWithoutNotify(index);
-            #endif
         }
 
         private void ChangeMicrophone(int index)
@@ -45,11 +31,10 @@ namespace Samples.Whisper
         {
             isRecording = true;
             recordButton.enabled = false;
-
-            var index = PlayerPrefs.GetInt("user-mic-device-index");
             
             #if !UNITY_WEBGL
-            clip = Microphone.Start(dropdown.options[index].text, false, duration, 44100);
+            string defaultMic = Microphone.devices[0];
+            clip = Microphone.Start(defaultMic, false, duration, 44100);
             #endif
         }
 
